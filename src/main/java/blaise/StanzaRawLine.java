95c31e803c1b4b89be1d74af762c90d4e5a3de28
Arnaud Roques <plantuml@gmail.com>
2023-05-01 04:35:40 +0200

    improve tests

--- src/main/java/blaise/StanzaRawLine.java
+++ src/main/java/blaise/StanzaRawLine.java
@@ -1,14 +1,26 @@
 package blaise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StanzaRawLine implements Stanza {
 
+    private final List<BToken> tokens = new ArrayList<>();
+
     public StanzaRawLine(Inspector<BToken> inspector) {
-        while (inspector.peek(0).getType() != BTokenType.END_OF_LINE)
+        while (inspector.peek(0).getType() != BTokenType.END_OF_LINE) {
+            tokens.add(inspector.peek(0));
             inspector.moveForward();
+        }
         inspector.moveForward();
     }
 
     public StanzaType getType() {
         return StanzaType.RAW;
+    }
+
+    @Override
+    public String toString() {
+        return tokens.toString();
     }
 }
--- src/test/java/blaise/BSourceBlocTest.java
+++ src/test/java/blaise/BSourceBlocTest.java
@@ -19,6 +19,11 @@ class BSourceBlocTest {
         final List<Stanza> stanzas = source.compileMe();
         Assertions.assertEquals(3, stanzas.size());
         Assertions.assertEquals(StanzaType.RAW, stanzas.get(0).getType());
+        Assertions.assertEquals("[IDENTIFIER a]", stanzas.get(0).toString());
+        Assertions.assertEquals(StanzaType.RAW, stanzas.get(1).getType());
+        Assertions.assertEquals("[IDENTIFIER b]", stanzas.get(1).toString());
+        Assertions.assertEquals(StanzaType.RAW, stanzas.get(2).getType());
+        Assertions.assertEquals("[IDENTIFIER c]", stanzas.get(2).toString());
 
     }
 
